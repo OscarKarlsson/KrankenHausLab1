6ef7dcc09c8e0f8db774d1fc37e6292d51d4c5bd
Desire Tillerås <desire.tilleras@gmail.com>
2021-03-07 15:50:49 +0100

    Lagt till Hospital klass med prop.

---
+++ KrankenHaus/Hospital/HospitalDepartments/Hospital.cs
@@ -0,0 +1,33 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hospital
+{
+    class Hospital
+    {
+        public List<Patient> PatientsInQueue { get; set; }        
+        public List<Doctor> DoctorsList { get; set; }
+        public Doctor CurrentDoctor { get; set; }
+        public int AmountPatientsAfterLife { get; set; }
+        public int AmountPatientsRecovered { get; set; }
+
+        public Hospital()
+        {
+            PatientsInQueue = new List<Patient>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                PatientsInQueue.Add(new Patient());
+            }
+
+            DoctorsList = new List<Doctor>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                DoctorsList.Add(new Doctor());
+            }
+        }
+    }
+}
--- KrankenHaus/Hospital/HospitalDepartments/IVA.cs
+++ KrankenHaus/Hospital/HospitalDepartments/IVA.cs
@@ -1,10 +1,18 @@
-﻿using System;
+﻿using Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Hospital.HospitalDepartments
 {
-    class IVA
+    class IVA : Hospital
     {
+        //IVA har fem platser
+        // För varje justering av sjukdomsnivån så har patienter på IVA 10 % risk att få en höjd sjukdomsnivå
+        // 20 % chans att sjukdomsnivån kvarstår
+        // 70 % chans att sjukdomsnivån minskar
+        public List<Patient> PatientsIVA { get; set; }
+
+
     }
 }
