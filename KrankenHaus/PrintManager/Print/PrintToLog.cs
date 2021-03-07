776e8f67743356a25a090680cd20a1403dac84b7
Desire Tillerås <desire.tilleras@gmail.com>
2021-03-07 14:53:58 +0100

    Dessi lagt till metod i PrintToLog

--- KrankenHaus/PrintManager/Print/PrintToLog.cs
+++ KrankenHaus/PrintManager/Print/PrintToLog.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
 
 namespace PrintManager.Print
 {
     class PrintToLog
     {
+        private string fileName = "KrankenHaus.txt";
+
+        public void WriteToFile(string text)
+        {
+            File.AppendAllText(fileName, text);
+        }        
     }
 }
