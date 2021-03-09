7050271f56d35059eb0b0f0c69a61029a7ca40f4
EmanuelKarlsson <emanuel.karlsson1@edu.varberg.se>
2021-03-09 08:10:17 +0100

    Commit method AssignPatientsToDepartments

--- KrankenHaus/Simulation/Simulator.cs
+++ KrankenHaus/Simulation/Simulator.cs
@@ -19,10 +19,50 @@ namespace Simulation
         }
         public void AssignPatientsToDepartments()
         {
-            //Kollat mot MaxIva och MaxSanatorium.
-            //Add patients to IVA and Sanatorium.
 
+            //Counting number of patients in IVA.
+            int patientsInIva = hospital.Patients.Where(p => p.Department == Departments.IVA).Count();
 
+            //Counting number of patients in Sanitorium.
+            int patientsInSanitorium = hospital.Patients.Where(p => p.Department == Departments.IVA).Count();
+
+            //Checking if patientsInIva is less than MaxIVA.
+            while (patientsInIva < MaxIVA)
+            {
+                //Iterating through patients list to find patients in queue. 
+                for (int i = 0; i < hospital.Patients.Count; i++)
+                {
+                    //Changing department to IVA and adding 1 to patientsInIva.
+                    if (hospital.Patients[i].Department == Departments.QUEUE)
+                    {
+                        if (hospital.Patients[i].Status == Status.Sick)
+                        {
+                            hospital.Patients[i].Department = Departments.IVA;
+                            patientsInIva++;
+                        }
+
+                    }
+                }
+            }
+
+            //Checking if patientsInSanitorium is less than MaxSanitorium.
+            while (patientsInSanitorium < MaxSanatorium)
+            {
+                //Iterating through patients list to find patients in queue. 
+                for (int i = 0; i < hospital.Patients.Count; i++)
+                {
+                    //Changing department to Sanitorium and adding 1 to patientsInSanitorium.
+                    if (hospital.Patients[i].Department == Departments.QUEUE)
+                    {
+                        if (hospital.Patients[i].Status == Status.Sick)
+                        {
+                            hospital.Patients[i].Department = Departments.SANATORIUM;
+                            patientsInSanitorium++;
+                        }
+
+                    }
+                }
+            }
         }
         public void AddDoctorToDepartment()
         {
