606a23c1c6cb22c2025d58619d08f6a139f0706a
Desire Tillerås <desire.tilleras@gmail.com>
2021-03-10 13:08:35 +0100

    .

--- KrankenHaus/PrintManager/Print/ConsolePrint.cs
+++ KrankenHaus/PrintManager/Print/ConsolePrint.cs
@@ -14,14 +14,19 @@ namespace PrintManager.Print
         int daycounter = 1;
         public void PrintFiveSeconds(object sender, ReportEventArgs args)
         {
-            
-            Console.WriteLine($"Day : {daycounter}\n" +
-                $"Amount of patients dead: {args.AmountDead}" +
-                $"\nAmount of patients recovered: {args.AmountRecovered}" +
-                $"\nAmount of patients in IVA: {args.AmountIVA}" +
-                $"\nAmount of patients in sanatorium: {args.AmountSanatorium}" +
-                $"\nAmount of patients in queue: {args.AmountPatientsInQueue}" +
-                $"\nDoctors waiting: {args.AmountDoctorsWaiting}\n\n");
+
+            StringBuilder write = new StringBuilder();
+            write.AppendLine($"Day : {daycounter}\n");
+            write.AppendLine($"Amount of patients dead: {args.AmountDead}");
+            write.AppendLine($"\nAmount of patients recovered: {args.AmountRecovered}");
+            write.AppendLine($"\nAmount of patients in IVA: {args.AmountIVA}");
+            write.AppendLine($"\nAmount of patients in sanatorium: {args.AmountSanatorium}");
+            write.AppendLine($"\nAmount of patients in queue: {args.AmountPatientsInQueue}");
+            write.AppendLine($"\nDoctors waiting: {args.AmountDoctorsWaiting}\n\n");
+            string textToFile = write.ToString();
+            PrintToLog printToLog = new PrintToLog();
+            printToLog.WriteToFile(textToFile);
+            Console.WriteLine(textToFile);
             daycounter++;
         }
     }
